88bba19e54e08da82dd06d065f569f4e8756f481
narangerelmunkhzul452-dev <narangerelmunkhzul452@gmail.com>
2025-09-15 16:23:11 +0900

    Add files via upload

---
+++ week3 20253012.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    int sum = 0;
+    int i;
+ 
+    for (i = 1; i <= 10; i++) { 
+        sum = sum + i;
+    }
+ 
+    printf("1부터 10까지의 합: %d\n", sum);
+ 
+    return 0;
+}
